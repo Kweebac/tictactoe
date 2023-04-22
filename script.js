16a80e41bd522a1e41cdd57851b798ee226020b8
Kweebac <06.amarshall@gmail.com>
2023-04-22 02:04:38 +0100

    Added functionality to check if you win or tie

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const gameBoard = (function () {
-  let gameBoard = [];
-  let currentPlayer;
+  let _gameBoard = [];
+  let _currentPlayer;
 
   const getGameBoard = function () {
-    return gameBoard;
+    return _gameBoard;
   };
   const addToGameBoard = function (marker, index) {
-    gameBoard[index] = marker;
+    _gameBoard[index] = marker;
   };
 
   const getCurrentPlayer = function () {
-    return currentPlayer;
+    return _currentPlayer;
   };
   const setCurrentPlayer = function (player) {
-    currentPlayer = player;
+    _currentPlayer = player;
   };
   const switchPlayer = function () {
-    currentPlayer !== player1
-      ? (currentPlayer = player1)
-      : (currentPlayer = player2);
+    _currentPlayer !== player1
+      ? (_currentPlayer = player1)
+      : (_currentPlayer = player2);
   };
 
   return {
@@ -31,38 +31,64 @@ const gameBoard = (function () {
 })();
 
 const displayControl = (function () {
-  const gridSquares = document.querySelectorAll(".grid > div");
-  const player1UIMarker = document.querySelector(
+  const _gridSquares = document.querySelectorAll(".grid > div");
+  const _player1UIMarker = document.querySelector(
     ".player:first-child > div:last-child"
   );
-  const player2UIMarker = document.querySelector(
+  const _player2UIMarker = document.querySelector(
     ".player:last-child > div:last-child"
   );
-  const player1UIName = document.querySelector(
+  const _player1UIName = document.querySelector(
     ".player:first-child > div:first-child"
   );
-  const player2UIName = document.querySelector(
+  const _player2UIName = document.querySelector(
     ".player:last-child > div:first-child"
   );
 
   const setMarkers = function () {
-    for (let i = 0; i < gridSquares.length; i++) {
-      gridSquares[i].textContent = gameBoard.getGameBoard()[i];
+    for (let i = 0; i < _gridSquares.length; i++) {
+      _gridSquares[i].textContent = gameBoard.getGameBoard()[i];
     }
   };
 
   const playerUI = function () {
-    player1UIMarker.textContent = player1.marker;
-    player2UIMarker.textContent = player2.marker;
-    player1UIName.textContent = player1.name;
-    player2UIName.textContent = player2.name;
+    _player1UIMarker.textContent = player1.marker;
+    _player2UIMarker.textContent = player2.marker;
+    _player1UIName.textContent = player1.name;
+    _player2UIName.textContent = player2.name;
   };
 
-  for (let i = 0; i < gridSquares.length; i++) {
-    gridSquares[i].addEventListener("click", () => {
-      if (gridSquares[i].textContent === "") {
+  const _isThreeInARow = function (a, b, c) {
+    return (
+      gameBoard.getGameBoard()[a] === gameBoard.getCurrentPlayer().marker &&
+      gameBoard.getGameBoard()[b] === gameBoard.getCurrentPlayer().marker &&
+      gameBoard.getGameBoard()[c] === gameBoard.getCurrentPlayer().marker
+    );
+  };
+
+  const _isGameOver = function () {
+    if (
+      _isThreeInARow(0, 1, 2) ||
+      _isThreeInARow(3, 4, 5) ||
+      _isThreeInARow(6, 7, 8) ||
+      _isThreeInARow(0, 3, 6) ||
+      _isThreeInARow(1, 4, 7) ||
+      _isThreeInARow(2, 5, 8) ||
+      _isThreeInARow(0, 4, 8) ||
+      _isThreeInARow(2, 4, 6)
+    ) {
+      console.log("You win");
+    } else if (gameBoard.getGameBoard().join("").length === 9) {
+      console.log("It's a tie");
+    }
+  };
+
+  for (let i = 0; i < _gridSquares.length; i++) {
+    _gridSquares[i].addEventListener("click", () => {
+      if (_gridSquares[i].textContent === "") {
         gameBoard.addToGameBoard(gameBoard.getCurrentPlayer().marker, i);
-        displayControl.setMarkers();
+        setMarkers();
+        _isGameOver();
         gameBoard.switchPlayer();
       }
     });
